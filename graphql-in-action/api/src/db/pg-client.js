a4b43d9cda8be765da4c417021f0a83e0295e20c
Henry Quinde <hrypasato@gmail.com>
2022-03-03 18:35:45 -0500

    test(pgConnection): change connection test with the db by count tables in the schema

--- graphql-in-action/api/src/db/pg-client.js
+++ graphql-in-action/api/src/db/pg-client.js
@@ -10,13 +10,14 @@ export default async function pgClient() {
   // Test the connection
   const client = await pgPool.connect();
   const tableCountResp = await client.query(
-    'select now();'
+    'select count(*) from information_schema.tables where table_schema = $1;',
+    ['azdev']
   );
   client.release();
 
   console.log(
-    'Connected to PostgreSQL | Now:',
-    tableCountResp.rows[0].now
+    'Connected to PostgreSQL | Tables count:',
+    tableCountResp.rows[0].count
   );
 
   pgPool.on('error', (err) => {
