04e5e0f4fd480c1accbcb30c8074fe47ddeda506
Henry Quinde <hrypasato@gmail.com>
2022-03-05 20:27:28 -0500

    refactor(queries): split queries into new file

--- graphql-in-action/api/src/schema/index.js
+++ graphql-in-action/api/src/schema/index.js
@@ -1,59 +1,8 @@
-import { GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
-import { numbersInRangeObject } from "../utils";
-import NumbersInRange from "./types/numbers-in-range";
-import Task from "./types/task";
-
-const QueryType = new GraphQLObjectType({
-    name: 'Query',
-    fields:{
-        currentTime: {
-            type: GraphQLString,
-            resolve: () => {
-                return new Promise(resolve => {
-                    setTimeout(() => {
-                        const isoString = new Date().toISOString();
-                        resolve(isoString.slice(11,19));
-                    }, 5000);
-                });
-            }
-        },
-        sumNumbersInRange:{
-            type: new GraphQLNonNull(GraphQLInt),
-            args:{
-                begin: {
-                    type: new GraphQLNonNull(GraphQLInt)
-                },
-                end:{
-                    type: new  GraphQLNonNull(GraphQLInt)
-                }
-            },
-            resolve: function( source, { begin, end }){
-                let sum = 0;
-                for(let i = begin; i <= end; i++){
-                    sum += i
-                }
-                return sum;
-            }
-        },
-        numbersInRange:{
-            type: NumbersInRange,
-            args:{
-                begin: { type: new  GraphQLNonNull(GraphQLInt) },
-                end: { type: new  GraphQLNonNull(GraphQLInt) }
-            },
-            resolve: function(source, { begin, end }){
-                return numbersInRangeObject(begin, end);
-            }
-        },
-        taskMainList:{
-            type: new GraphQLList(new GraphQLNonNull(Task)),
-            resolve: async (source, args, { pgApi }) => {
-                return pgApi.taskMainList();
-            }
-        }
-    }
-});
+import { GraphQLSchema, printSchema } from "graphql";
+import QueryType from "./queries";
 
 export const schema = new GraphQLSchema({
     query: QueryType
 })
+
+console.log(printSchema(schema));
---
+++ graphql-in-action/api/src/schema/queries.js
@@ -0,0 +1,57 @@
+import { GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import NumbersInRange from './types/numbers-in-range';
+import { numbersInRangeObject } from '../utils';
+import Task from "./types/task";
+
+const QueryType = new GraphQLObjectType({
+    name: 'Query',
+    fields:{
+        currentTime: {
+            type: GraphQLString,
+            resolve: () => {
+                return new Promise(resolve => {
+                    setTimeout(() => {
+                        const isoString = new Date().toISOString();
+                        resolve(isoString.slice(11,19));
+                    }, 5000);
+                });
+            }
+        },
+        sumNumbersInRange:{
+            type: new GraphQLNonNull(GraphQLInt),
+            args:{
+                begin: {
+                    type: new GraphQLNonNull(GraphQLInt)
+                },
+                end:{
+                    type: new  GraphQLNonNull(GraphQLInt)
+                }
+            },
+            resolve: function( source, { begin, end }){
+                let sum = 0;
+                for(let i = begin; i <= end; i++){
+                    sum += i
+                }
+                return sum;
+            }
+        },
+        numbersInRange:{
+            type: NumbersInRange,
+            args:{
+                begin: { type: new  GraphQLNonNull(GraphQLInt) },
+                end: { type: new  GraphQLNonNull(GraphQLInt) }
+            },
+            resolve: function(source, { begin, end }){
+                return numbersInRangeObject(begin, end);
+            }
+        },
+        taskMainList:{
+            type: new GraphQLList(new GraphQLNonNull(Task)),
+            resolve: async (source, args, { pgApi }) => {
+                return pgApi.taskMainList();
+            }
+        }
+    }
+});
+
+export default QueryType;
