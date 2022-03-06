dbecb13919829ed57861e5e014fdaa36944b6308
Henry Quinde <hrypasato@gmail.com>
2022-03-05 21:07:40 -0500

    feat(server): add task field for Approach type object

--- graphql-in-action/api/src/schema/types/approach.js
+++ graphql-in-action/api/src/schema/types/approach.js
@@ -1,9 +1,10 @@
 import { GraphQLID, GraphQLInt, GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import Task from "./task";
 import User from "./user";
 
 const Approach = new GraphQLObjectType({
     name:'Approach',
-    fields:{
+    fields: () => ({
         id: { type:new GraphQLNonNull(GraphQLID) },
         content: { type: new GraphQLNonNull(GraphQLString) },
         voteCount: { type: new GraphQLNonNull(GraphQLInt) },
@@ -14,8 +15,12 @@ const Approach = new GraphQLObjectType({
         author:{
             type: new GraphQLNonNull(User),
             resolve: (source, args, { loaders }) => loaders.users.load(source.userId)
+        },
+        task:{
+            type: new GraphQLNonNull(Task),
+            resolve: (source, args, { loaders }) => loaders.tasks.load(source.taskId)
         }
-    }
+    })
 });
 
 export default Approach;
