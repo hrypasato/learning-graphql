6ebe0af3aa4a5293b7f4066750d3a0151080f926
Henry Quinde <hrypasato@gmail.com>
2022-03-07 18:40:29 -0500

    feat(type): implement approach detail object type

---
+++ graphql-in-action/api/src/schema/types/approach-detail.js
@@ -0,0 +1,12 @@
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import ApproachDetailCategory from "./approach-detail-category";
+
+const ApproachDetail = new GraphQLObjectType({
+    name: 'ApproachDetail',
+    fields: () => ({
+        content:{ type: new GraphQLNonNull(GraphQLString) },
+        category: { type: new GraphQLNonNull(ApproachDetailCategory) }
+    })
+});
+
+export default ApproachDetail;
