2d6aa257df3862ce070c73fff730abc62d247c34
Henry Quinde <hrypasato@gmail.com>
2022-03-08 22:20:01 -0500

    feat(schema): create AuthInput input type

---
+++ graphql-in-action/api/src/schema/types/input-auth.js
@@ -0,0 +1,11 @@
+import { GraphQLInputObjectType, GraphQLNonNull, GraphQLString } from "graphql";
+
+const AuthInput = new GraphQLInputObjectType({
+    name:'AuthInput',
+    fields: ()=>({
+        username: { type: new GraphQLNonNull(GraphQLString) },
+        password: { type: new GraphQLNonNull(GraphQLString) }
+    })
+});
+
+export default AuthInput;
